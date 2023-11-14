840a94d01832d6d76bc6d9819464a46b4b40c4a5
Balaji Kamineni <105938320+Balaji-Kamineni@users.noreply.github.com>
2023-11-14 06:00:18 +0000

    conversion to Boolean

---
+++ js/Day02/conversionToBoolean.js
@@ -0,0 +1,52 @@
+    let score,value;
+    /* converting  number to boolean */
+
+// score=103;
+// console.log(typeof score); //number
+// value=Boolean(score);
+// console.log(value); //true
+// console.log(typeof value); //boolean
+
+// score=0;
+// console.log(typeof score); //number
+// value=Boolean(score);
+// console.log(value); //false
+// console.log(typeof value); //boolean
+
+    /* converting string to boolean */
+
+// score="";
+// console.log(typeof score);  //string
+// value=Boolean(score);
+// console.log(typeof value); //boolean
+// console.log(value); //false
+
+
+// score=" ";
+// console.log(typeof score);  //string
+// value=Boolean(score);
+// console.log(typeof value); //boolean
+// console.log(value); //true
+
+
+// score="Balaji";
+// console.log(typeof score);  //string
+// value=Boolean(score);
+// console.log(typeof value); //boolean
+// console.log(value); //true
+
+    /* converting null to boolean */
+
+// score=null;
+// console.log(typeof score); //object
+// value=Boolean(score);
+// console.log(value); //false
+// console.log(typeof value); //Boolean
+
+    /* Converting undefined to boolean */
+
+// console.log(typeof score);  //undefined
+// console.log(score); //undefined
+// value=Boolean(score); 
+// console.log(typeof value); //boolean
+// console.log(value); //false
