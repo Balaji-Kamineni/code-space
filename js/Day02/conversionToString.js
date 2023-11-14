6fd9c640587d984787fa556f9024f3c9184eed26
Balaji Kamineni <105938320+Balaji-Kamineni@users.noreply.github.com>
2023-11-14 05:25:51 +0000

    conversion to string

---
+++ js/Day02/conversionToString.js
@@ -0,0 +1,44 @@
+//let score=33;
+//console.log(typeof score);  //number
+
+        /* converting number to string */
+// let value=String(score);
+// console.log(value);   // "33"
+// console.log(typeof value); //string
+
+        /* convering string to string */
+
+// score="33";
+// console.log(typeof score); //string
+// let value=String(score); 
+// console.log(typeof value); //string
+// console.log(value); //"33"
+
+        /* converting Boolean to String */
+
+// score=true;
+// console.log(typeof score); //boolean
+// let value=String(score);
+// console.log(value[0]); // t
+// console.log(value); //true
+// console.log(typeof value); //string
+
+        /* converting null to String */
+
+// score=null;
+// console.log(typeof score); //object
+// let value=String(score);
+// console.log(value[0]); //n
+// console.log(value); //null
+// console.log(typeof value); //string
+
+        /* Converting undefined to string */
+
+// let score;
+// console.log(typeof score);
+// value=String(score);
+// console.log(value); //undefined
+// console.log(value[2]); //d
+// console.log(typeof value); //string
+
+
